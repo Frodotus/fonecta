340063e96fa57daa8149a791035daae5749c48c9
Toni Leino <toni@leino.net>
2013-02-12 10:57:02 +0200

    Sinatra app

--- fonecta.rb
+++ fonecta.rb
@@ -1,18 +1,26 @@
 require 'rubygems'
+require 'sinatra'
 require 'mechanize'
 
-a = Mechanize.new
-a.get('https://m.fonecta.fi/kirjaudu?from=%2Fhenkilot') do |login_page|
-  search_page = login_page.form_with(:action => '/kirjaudu') do |f|
-    f.username  = ARGV[0]
-    f.password  = ARGV[1]
-  end.click_button
-  result_page = search_page.form_with(:action => '/henkilot/haku') do |f|
-    f.what  = ARGV[2]
-  end.click_button
-  result = result_page.at '.search-results'
-  name = result.at('.profile').text.strip   
-  address = result.at('.adr').text.strip   
-  puts "Name: '#{name}'"
-  puts "Address: '#{address}'"
+get '/search/:username/:password/:number' do
+  fetch params[:username],params[:password],params[:number]
+end
+
+private
+
+def fetch(username,password,number)
+  a = Mechanize.new
+  a.get('https://m.fonecta.fi/kirjaudu?from=%2Fhenkilot') do |login_page|
+    search_page = login_page.form_with(:action => '/kirjaudu') do |f|
+      f.username  = username
+      f.password  = password
+    end.click_button    
+    result_page = search_page.form_with(:action => '/henkilot/haku') do |f|
+      f.what  = number
+    end.click_button
+    result = result_page.at '.search-results'
+    name = result.at('.profile').text.strip
+    address = result.at('.adr').text.strip
+    return "<Name>#{name}</Name>\n<LastName></LastName>\n<Address>#{address}</Address>\n<PostalCode></PostalCode>\n<City></City>\n"
+  end
 end
